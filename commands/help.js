84f13aad692e5aa09dbc11cd53bb1f60b9bb31ad
Ritik Ramuka <ritik.ramuka@gmail.com>
2021-03-04 21:52:22 +0530

    update

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,23 @@
+const { green, yellowBright } = require("chalk");
 let chalk = require("chalk");
 function help() {
   console.log(
     chalk.redBright.bold(
       `List of all the commands:` +
-        chalk.keyword("orange")(
-          `
+      chalk.keyword("orange")(
+        `
     rikrak view <dir-name> flat
     rikrak view <dir-name> tree
     rikrak organize <dir-name>
-    rikrak organize 
-    rikrak help
+    rikrak organize \n`
+        + chalk.keyword("white")(
+          `\n    To Revert Organize Command`
+        ) + `
+    rikrak disorganize <dir-name>
+    rikrak disorganize 
+    \n    rikrak help
   `
-        )
+      )
     )
   );
 }
--- rikrak.js
+++ rikrak.js
@@ -5,6 +5,8 @@ let command = input[0];
 let helpFile = require("./commands/help");
 let viewFile = require("./commands/view");
 let organizeFile = require("./commands/organize");
+let disorganizeFile = require("./commands/disorganize");
+let chalk = require("chalk");
 switch (command) {
   case "view":
     viewFile.fn(input[1], input[2]);
@@ -13,10 +15,14 @@ switch (command) {
     if (input.length == 1) organizeFile.fn(process.cwd());
     else organizeFile.fn(input[1]);
     break;
+  case "disorganize":
+    if (input.length == 1) disorganizeFile.fn(process.cwd());
+    else disorganizeFile.fn(input[1]);
+    break;
   case "help":
     helpFile.fn();
     break;
   default:
-    console.log("invalid command run rikrak help command for commands");
+    console.log(chalk.magentaBright.bold("invalid command run rikrak help command for commands"));
     break;
 }
